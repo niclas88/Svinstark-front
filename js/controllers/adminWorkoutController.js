47be145ea6a690783f612de5b4189aea87dab17c
Niclas88 <niclas88@hotmail.se>
2016-02-23 23:20:25 +0100

    Changes in adminWorkout

--- js/controllers/adminWorkoutController.js
+++ js/controllers/adminWorkoutController.js
@@ -2,6 +2,8 @@ myApp.controller("adminWorkoutController", ["$scope", "$http", function($scope, 
     
 	$scope.LimitedResult = {};
     // $scope.listOfItems = ["Bench", "Deadlift", "Squats", "Military Press"]
+    $scope.newExercise =  {};
+    $scope.selectedMuscleGroup = {};
     
     $http.get("http://localhost:58023/api/admin/getmusclegroups").success(function(data){
         $scope.muscleGroups = data;
@@ -11,8 +13,20 @@ myApp.controller("adminWorkoutController", ["$scope", "$http", function($scope, 
         $scope.listOfItems = data;
     });
     
+    $scope.PostExercise = function(){
+        $http.post("http://localhost:58023/api/admin/postexercise", {
+            ExerciseId: $scope.newExercise.Id,
+            ExerciseName: $scope.newExercise.Name,
+            MuscleGroupId: $scope.selectedMuscleGroup.Id,
+            MuscleGroupName: $scope.selectedMuscleGroup.Name
+        }).success(function(data){
+            $scope.listOfItems = data;
+        });
+    }
+    
     $scope.exerciseMuscleGroup = "Select primary muscle group";
     $scope.selectNewExerciseMuscleGroup =  function(item){
         $scope.exerciseMuscleGroup = item.Name;
+        $scope.selectedMuscleGroup = item;
     }
 }]);
