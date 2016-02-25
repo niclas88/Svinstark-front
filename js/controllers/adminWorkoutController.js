0f1638d73edb3a66da104e97e8a06c139fc8f434
Niclas88 <niclas88@hotmail.se>
2016-02-25 17:18:59 +0100

    Changes in admin

--- js/controllers/adminWorkoutController.js
+++ js/controllers/adminWorkoutController.js
@@ -3,7 +3,6 @@ myApp.controller("adminWorkoutController", ["$scope", "$http", function($scope, 
     $scope.newExercise =  {};
     $scope.newMuscleGroup = {};
     $scope.selectedMuscleGroup = {};
-    $scope.exercises = {};
     
     $http.get("http://localhost/Sm%C3%A5flickor/api/admin/getmusclegroups").success(function(data){
         $scope.muscleGroups = data;
@@ -13,6 +12,7 @@ myApp.controller("adminWorkoutController", ["$scope", "$http", function($scope, 
         $scope.exercises = data;
     });
     
+    $("#success-exercise").hide();
     $scope.PostExercise = function(){
         $http.post("http://localhost/Sm%C3%A5flickor/api/admin/postexercise", {
             Id: $scope.newExercise.Id,
@@ -22,18 +22,36 @@ myApp.controller("adminWorkoutController", ["$scope", "$http", function($scope, 
             } 
         }).success(function(data){
             $scope.exercises = data;
+            $("#success-exercise").alert();
+                $("#success-exercise").fadeTo(2000, 500).slideUp(500, function(){
+                    $("#success-exercise").hide();
+                });
+        }).catch(function(data){
+            $scope.exerciseModelState = data.data.ModelState;
         });
     }
     
-    $("#success-alert").hide();
+    $scope.DeleteExercise = function(item) {
+        console.log(item);
+        $http.delete("http://localhost/Sm%C3%A5flickor/api/admin/DeleteExercise",{
+            Id: item.Id,
+            Name: item.Name,
+        }).success(function(data){
+            $scope.exercises = data;
+        }).catch(function(data){
+            
+        });
+    };
+    
+    $("#success-muscle").hide();
     $scope.PostMuscleGroup = function(){
         $http.post("http://localhost/Sm%C3%A5flickor/api/admin/postmusclegroup", {
             Name: $scope.newMuscleGroup.Name
         }).success(function(data){
            $scope.muscleGroups = data;
-           $("#success-alert").alert();
-                $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
-                    $("#success-alert").hide();
+           $("#success-muscle").alert();
+                $("#success-muscle").fadeTo(2000, 500).slideUp(500, function(){
+                    $("#success-muscle").hide();
                 });
         });
     }
